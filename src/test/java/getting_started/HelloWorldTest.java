ca032bb8a5f7bc75b88209e4dadc33ae1c1ef979
JonEG <echevestegonzalezjon@gmail.com>
2026-06-04 11:06:08 +0200

    Add code formatters and format current files

--- src/test/java/getting_started/HelloWorldTest.java
+++ src/test/java/getting_started/HelloWorldTest.java
@@ -13,30 +13,30 @@ import static org.slf4j.LoggerFactory.getLogger;
 import java.lang.invoke.MethodHandles;
 
 public class HelloWorldTest {
-	
+
 	static final Logger log = getLogger(MethodHandles.lookup().lookupClass());
-	
+
 	private WebDriver driver;
-	
+
 	@BeforeAll
 	static void setupClass() {
 		WebDriverManager.chromedriver().setup();
 	}
-	
+
 	@BeforeEach
 	void setup() {
 		driver = new ChromeDriver();
 	}
-	
+
 	@Test
 	void test() {
 		driver.get("https://github.com/JonEG");
-		
+
 		log.info("You have visited: {}", driver.getTitle());
-		
+
 		assertThat(driver.getTitle()).contains("Jon Echeveste");
 	}
-	
+
 	@AfterEach
 	void teardown() {
 		driver.quit();
--- src/test/java/getting_started/LocationStrategyTest.java
+++ src/test/java/getting_started/LocationStrategyTest.java
@@ -19,60 +19,59 @@ import org.slf4j.Logger;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class LocationStrategyTest {
-	
+
 	static final Logger log = getLogger(MethodHandles.lookup().lookupClass());
-	
+
 	private WebDriver driver;
-	
+
 	@BeforeAll
 	static void setupClass() {
 		WebDriverManager.chromedriver().setup();
 	}
-	
+
 	@BeforeEach
 	void setup() {
 		driver = new ChromeDriver();
 		driver.get("https://www.selenium.dev/selenium/web/web-form.html");
 	}
-	
+
 	@AfterEach
 	void teardown() {
 		driver.quit();
 	}
-		
+
 	@Test
 	void testLocateByTagName() {
 		WebElement textarea = driver.findElement(By.tagName("textarea"));
 		assertThat(textarea.getDomAttribute("rows")).isEqualTo("3");
 	}
-	
+
 	@Test
 	void testLocateByHtmlAttributes() {
 		// By name
 		WebElement textByName = driver.findElement(By.name("my-text"));
 		assertThat(textByName.isDisplayed());
-		
+
 		// By id
 		WebElement textById = driver.findElement(By.id("my-text-id"));
 		assertThat(textById.getAttribute("type")).isEqualTo("text");
 		assertThat(textById.getDomAttribute("type")).isEqualTo("text");
 		assertThat(textById.getDomProperty("type")).isEqualTo("text");
-		
+
 		// By class name
 		List<WebElement> byClassName = driver.findElements(By.className("form-control"));
 		assertThat(byClassName.size()).isPositive();
 	}
-	
-	@Test 
+
+	@Test
 	void testLocateByLinkText() {
 		WebElement linkByText = driver.findElement(By.linkText("Return to index"));
 		assertThat(linkByText.getTagName()).isEqualTo("a");
 		assertThat(linkByText.getCssValue("text-decoration")).isEqualTo("underline");
-		
+
 		WebElement linkByPartialText = driver.findElement(By.partialLinkText("index"));
 		assertThat(linkByPartialText.getLocation()).isEqualTo(linkByText.getLocation());
 		assertThat(linkByPartialText.getRect()).isEqualTo(linkByText.getRect());
 	}
-	
-	
+
 }
